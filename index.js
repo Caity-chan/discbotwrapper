8c235b04d04f1d8642e933a13aedf045cfa06c62
Caity-chan <74330895+Caity-chan@users.noreply.github.com>
2021-03-15 08:48:11 -0700

    added example usage code

--- index.js
+++ index.js
@@ -1,142 +1,65 @@
-const axios = require('axios');
-const WebSocket = require('ws');
-let token;
-let ws;
-let prefix;
-const status_stuff = {
-	"op": 3,
-	"d": {
-		"since": 91879201,
-		"activities": [{
-			"name": "Save the Oxford Comma",
-			"type": 0
-		}],
-		"status": "online",
-		"afk": false
-	}
-}
+startup_channel = 'channel_id_here';
 
-//let start = () => {};
-//let message_event = () => {};
-const event = {};
-const events = {};
-event.create = (name, func) => {
-	events[name] = func;
-}
-const command = {};
-const commands = {};
-const permissions = {};
-command.permissions = {};
+const { event, send_message, modify_status, login, command } = require('./wrapper');
+const express = require('express');
+const app = express();
+const env = require('dotenv');
 
-command.prefix = (prfx) => { prefix = prfx; }
-command.create = (name, func, perms) => {
-	commands[name] = func;
-	if (perms) permissions[name] = perms;
-}
-command.execute = (message, name) => {
-	if (!name) name = message.content.slice(prefix.length).split(' ')[0];
-	args = message.content.slice(prefix.length-1).split(' ').slice(1);
-	if (commands[name]) commands[name](message, args);
-}
-command.permissions.add = (name, perms) => {
-	for (const perm of perms) {
-		permissions[name].push(perm);
-	}
-}
-command.permissions.set = (name, perms) => {
-	permissions[name] = perms;
-} 
-modify_status = (arr) => {
-	for (const [key, value] of arr) {
-		switch (key) {
-			case 'since':
-				status_stuff['d']['since'] = value;
-				break;
-			
-			case 'status_message':
-				status_stuff['d']['activities'][0]['name'] = value;
-				break;
-			
-			case 'status_type':
-				status_stuff['d']['activities'][0]['type'] = value;
-				break;
-			
-			case 'appearance':
-				status_stuff['d']['status'] = value;
-				break;
-			
-			case 'afk':
-				status_stuff['d']['afk'] = value;
-				break;
-		}
-	}
-	ws.send(JSON.stringify(status_stuff));
-} 
 
-send_message = async ({message, tts, channel, embed}) => {
-	data = {
-			"content": message,
-			"tts": tts
-		}
-	if (embed) data.embed = embed;
-	await axios.post(`https://discord.com/api/channels/${channel}/messages`,
-		data
-		,
-		{headers: {
-			"Authorization": `Bot ${token}`
-		}}
-			
-	)
-}
-
-login = async (tkn) => {
-	token = tkn;
-	ws = new WebSocket('wss://gateway.discord.gg/?v=6&encoding=json');
-	let interval_set = false;
-	ws.on('message', async function incoming(data) {
-		data = JSON.parse(data);
-		msg = data.d;
-		_event = data.t;
-
-		if (_event === null) {
-			if (interval_set === false) {
-				setInterval(
-					function () {
-						ws.send('{"op": 1, "d": null}')
-					}, 
-					msg.heartbeat_interval
-				);
-					ws.send(`
-						{
-							"op": 2,
-							"d": {
-								"token": "${token}",
-								"intents": 513,
-								"properties": {
-									"$os": "linux",
-									"$browser": "my_library",
-									"$device": "my_library"
-								}
-							}
-						}
-					`);
-				interval_set = true;
-			} else {
-			}
-		} else {
-			//console.log(_event);
-			if (events[_event]) events[_event](data.d);
-			
-		}
+event.create('READY', async () => {
+	console.log('boop')
+	await send_message({
+		message: 'beep boop bop',
+		tts: false,
+		channel: startup_channel
 	});
-} 
-	
+	await modify_status(
+		[
+			['appearance', 'dnd'],
+			['status_message', 'eklfjsa;dja']
+		]
+	);
+});
+
+command.prefix('boop.')
+
+command.create('test', async (message, args) => {
+	await send_message({
+		message: 'hello hello blah blah blah',
+		channel: message.channel_id,
+		tts: false
+	});
+});
+
+command.create('tester', async (message, args) => {
+	await send_message({
+		message: 'hello hello basfkld;fjasdlkafjsdlah blah blah',
+		channel: message.channel_id,
+		tts: false
+	});
+});
+
+command.create('message_info', async (message, args) => {
+	await send_message({
+		message: JSON.stringify(message),
+		channel: message.channel_id,
+		tts: false
+	});
+})
+
+command.create('member_info', async (message, args) => {
+	await send_message({
+		message: JSON.stringify(message.member),
+		channel: message.channel_id,
+		tts: false
+	});
+});
 
 
+event.create('MESSAGE_CREATE', async (message) => {
+	if (message.content.toLowerCase().startsWith('boop.')) {
+		command.execute(message);
+	};
+});
 
-
-module.exports.event = event;
-module.exports.send_message = send_message;
-module.exports.modify_status = modify_status;
-module.exports.login = login;
-module.exports.command = command;
+login(process.env.TOKEN);
